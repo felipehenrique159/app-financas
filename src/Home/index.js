65d5820cf27bdecf2f64f902c37d66e340269b5d
felipehenrique159 <felipecod6@hotmail.com>
2024-03-24 22:54:33 -0300

    feat: componente Header

---
+++ src/Home/index.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import Header from '../components/Header'
+
+export default function index() {
+  return (
+    <View>
+        <Header name="Felipe Ananias"/>
+      <Text>Pagina Home</Text>
+    </View>
+  )
+}
---
+++ src/components/Header/index.js
@@ -0,0 +1,48 @@
+import { View, Text , StyleSheet, StatusBar, TouchableOpacity} from 'react-native'
+import React from 'react'
+import { Feather } from '@expo/vector-icons'
+
+const statusBarHeight = StatusBar.currentHeight ? StatusBar.currentHeight + 22 : 64
+
+export default function Header({name}) {
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.content}>
+        <Text style={styles.username}> {name}</Text>
+        <TouchableOpacity activeOpacity={0.9} style={styles.buttonUser}>
+            <Feather name='user' size={27} color='#FFF'/>
+        </TouchableOpacity>
+      </View>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+    container: {
+      backgroundColor: '#8000ff',
+      paddingTop: statusBarHeight,
+      flexDirection: 'row',
+      paddingStart: 16,
+      paddingEnd: 16,
+      paddingBottom: 44,
+    },
+    content: {
+        flex: 1,
+        flexDirection: 'row',
+        justifyContent: 'space-between'
+    },
+    username: {
+        fontSize: 18,
+        fontWeight: 'bold',
+        color: '#FFF'
+    },
+    buttonUser:{
+        width: 44,
+        height: 44,
+        backgroundColor: 'rgba(255,255,255, 0.5)',
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderRadius: 44/2
+    }
+  });
