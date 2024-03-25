91ba4752af8eb65d359aaab4dbbfb8b55aadae8f
felipehenrique159 <felipecod6@hotmail.com>
2024-03-24 23:12:47 -0300

    feat: componente balance

--- src/Home/index.js
+++ src/Home/index.js
@@ -1,12 +1,20 @@
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet } from 'react-native'
 import React from 'react'
 import Header from '../components/Header'
+import Balance from '../components/Balance'
 
-export default function index() {
+export default function Home() {
   return (
-    <View>
+    <View style={styles.container}>
         <Header name="Felipe Ananias"/>
-      <Text>Pagina Home</Text>
+        <Balance balance="120,00" expenses="-650,00"/>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fafafa'
+  },
+});
---
+++ src/components/Balance/index.js
@@ -0,0 +1,65 @@
+import { View, Text , StyleSheet} from 'react-native'
+import React from 'react'
+
+export default function Balance({balance, expenses}) {
+  return (
+    <View style={styles.container}>
+      <View style={styles.item}>
+        <Text style={styles.itemTitle}>Saldo</Text>
+        <View style={styles.content}>
+            <Text style={styles.currencySymbol}>R$</Text>
+            <Text style={styles.balance}>
+                {balance}
+            </Text>
+        </View>
+      </View>
+
+      <View style={styles.item}>
+        <Text style={styles.itemTitle}>Gastos</Text>
+        <View style={styles.content}>
+            <Text style={styles.currencySymbol}>R$</Text>
+            <Text style={styles.expenses}>
+                {expenses}
+            </Text>
+        </View>
+      </View>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+    container: {
+      backgroundColor: '#fff',
+      flexDirection: 'row',
+      justifyContent: 'space-between',
+      marginTop: -24,
+      marginStart: 14,
+      marginEnd: 14,
+      paddingTop: 22,
+      paddingStart: 18,
+      paddingEnd: 18,
+      paddingBottom: 22,
+      borderRadius: 4,
+      zIndex: 99
+    },
+    itemTitle: {
+        fontSize: 20,
+        color: '#dadada'
+    },
+    content: {
+        flexDirection: 'row',
+        alignItems: 'center'
+    },
+    currencySymbol: {
+        color: '#dadada',
+        marginRight: 6
+    },
+    balance: {
+        fontSize: 22,
+        color: '#2ecc71'
+    },
+    expenses:{
+        fontSize: 22,
+        color: 'red'
+    }
+});
